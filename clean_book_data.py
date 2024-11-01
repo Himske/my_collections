284763cf8499c96d81e5d03cc3adb02f7cd83578
Fredrik Nilsson <himske@gmail.com>
2024-11-01 11:21:04 +0100

    Extract authors and book data

--- clean_book_data.py
+++ clean_book_data.py
@@ -1,15 +1,23 @@
 import polars as pl
 
-q = pl.scan_csv("book_export_df2.csv")
+books = pl.scan_csv("book_export_df2.csv").collect().with_row_index("id")
 
-df = q.collect()
+books_authors = (
+    books.select(pl.col("id"), pl.col("author"))
+    .with_columns(pl.col("author").str.split("; ").alias("split_author"))
+    .explode("split_author")
+).select(pl.col("id").alias("book_id"), pl.col("split_author").alias("author"))
 
-authors = df.select(pl.col("author")).unique()
+authors = books_authors.select(pl.col("author")).unique().sort(by="author").with_row_index("id")
 
-authors = authors.with_columns(pl.col("author").str.split("; ").alias("split_author")).explode("split_author")
+books_authors = books_authors.join(authors, on="author", how="inner").select(
+    pl.col("book_id"), pl.col("id").alias("author_id")
+)
 
-authors = authors.select(pl.col("split_author").alias("author")).unique().sort(by="author").with_row_index("id")
-
+print(books.head(5))
 print(authors.head(10))
+print(books_authors.head(5))
 
+books.write_csv("books.csv")
 authors.write_csv("authors.csv")
+books_authors.write_csv("books_authors.csv")
